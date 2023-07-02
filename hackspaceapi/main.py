e2eb85e15e3ef0c4777e2e2431270b0a522e370f
Andrew Williams <andy@tensixtyone.com>
2023-07-02 06:05:45 +0100

    Add CORS headers

--- hackspaceapi/main.py
+++ hackspaceapi/main.py
@@ -2,12 +2,21 @@
 import logging
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .events import events
 from .spaceapi import spaceapi
 
 logging.basicConfig(level=logging.DEBUG)
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(spaceapi)
 app.include_router(events)
