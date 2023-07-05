638a5e174a8534ab6724f794ea6c515bc7fcdc25
Andrew Williams <andy@tensixtyone.com>
2023-07-05 07:46:18 +0100

    Skip prometheus sensors when we have no results

--- hackspaceapi/spaceapi.py
+++ hackspaceapi/spaceapi.py
@@ -123,7 +123,7 @@ def get_sensors() -> dict:
 
     for query, name, sensor_type in PROMETHEUS_SENSORS:
         data = get_prometheus_metric(query)
-        if not data:
+        if not data or len(data['results'] == 0):
             logging.warning('Call for {0} sensor returned an empty result, skipping'.format(name))
             continue
 
