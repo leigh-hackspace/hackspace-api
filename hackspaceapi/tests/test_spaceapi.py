6187405e9761eb1300fa5b0c48e133ed59b38dd2
Andrew Williams <andy@tensixtyone.com>
2023-07-20 17:06:57 +0100

    Fix tests

--- hackspaceapi/tests/test_spaceapi.py
+++ hackspaceapi/tests/test_spaceapi.py
@@ -1,22 +1,5 @@
 from hackspaceapi.tests.utils import FastAPIVCRTestCase, client
-from spacedirectory.directory import get_space_from_data
-from spacedirectory.space import Space
-
-
-class SpaceAPITestCase(FastAPIVCRTestCase):
-    """
-    Test the SpaceAPI with recorded backend interactions
-    """
-
-    cassette_name = "SpaceAPITestCase.backend_calls.yaml"
-
-    def test_space_json(self):
-        response = client.get("/space.json")
-        assert response.status_code == 200
-        assert response.headers["Content-Type"] == "application/json"
-
-        # Check we provide a valid SpaceAPI JSON
-        assert type(get_space_from_data(response.json())) == Space
+from spacedirectory.models.space import Space
 
 
 class SpaceAPINoNetworkTestCase(FastAPIVCRTestCase):
@@ -34,13 +17,31 @@ class SpaceAPINoNetworkTestCase(FastAPIVCRTestCase):
         data = response.json()
 
         # Check we provide a valid SpaceAPI JSON
-        assert type(get_space_from_data(data)) == Space
+        assert type(Space(data)) == Space
 
         # We should have no sensors
-        assert len(data['sensors']) == 0
+        assert len(data["sensors"]) == 0
 
         # Our status should be closed
-        assert data['state']['open'] is False
+        assert data["state"]["open"] is False
 
         # Last change shouldn't be in the state
-        assert 'lastchange' not in data['state']
+        assert "lastchange" not in data["state"]
+
+
+class SpaceAPITestCase(FastAPIVCRTestCase):
+    """
+    Test the SpaceAPI with recorded backend interactions
+    """
+
+    cassette_name = "SpaceAPITestCase.backend_calls.yaml"
+
+    def test_space_json(self):
+        response = client.get("/space.json")
+        assert response.status_code == 200
+        assert response.headers["Content-Type"] == "application/json"
+
+        data = response.json()
+
+        # Check we provide a valid SpaceAPI JSON
+        assert type(Space(data)) == Space
