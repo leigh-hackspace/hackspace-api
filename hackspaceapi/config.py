77a24d531bcdfe42e19120b035ada94a1f4d032b
Andrew Williams <andy@tensixtyone.com>
2023-07-22 08:30:22 +0100

    Document out the settings

--- hackspaceapi/config.py
+++ hackspaceapi/config.py
@@ -1,27 +1,77 @@
+from pydantic import Field, AnyHttpUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
 
-    base_url: str = "http://localhost:8000"
-    prometheus_instance: str = "http://prometheus:9090"
-    homeassistant_instance: str = "http://homeassistant:8123"
-    homeassistant_token: str
+    base_url: str = Field(
+        default="http://localhost:8000",
+        description="URL base where the application will be accessible at",
+    )
 
-    hackspace_name: str = "Leigh Hackspace"
-    hackspace_logo_url: str = "https://raw.githubusercontent.com/leigh-hackspace/logos-graphics-assets/master/logo/rose_logo.svg"
-    hackspace_website_url: str = "https://leighhack.org"
-    hackspace_address: str = "Leigh Hackspace, Unit 3.14, 3rd Floor, Leigh Spinners Mill, Park Lane, Leigh, WN7 2LB, United Kingdom"
-    hackspace_address_lat: float = 53.493012
-    hackspace_address_lon: float = -2.493010
-    hackspace_timezone: str = "Europe/London"
+    prometheus_instance: str = Field(
+        default="http://prometheus:9090",
+        description="Endpoint URL for the Prometheus instance",
+    )
 
-    hackspace_open_entity: str = "binary_sensor.hackspace_open_multi"
-    hackspace_public_calendar: str = "calendar.public_events"
-    hackspace_member_calendar: str = "calendar.member_events"
+    homeassistant_instance: str = Field(
+        default="http://homeassistant:8123",
+        description="Endpoint URL for the Home Assistant instance",
+    )
 
-    sensors_pressure_enabled: bool = False
+    homeassistant_token: str = Field(
+        description="Token used to access the Home Assistant API"
+    )
+
+    hackspace_name: str = Field(
+        default="Leigh Hackspace", description="Name of the hackspace"
+    )
+
+    hackspace_logo_url: str = Field(
+        default="https://raw.githubusercontent.com/leigh-hackspace/logos-graphics-assets/master/logo/rose_logo.svg",
+        description="URL to the logo for the hackspace",
+    )
+
+    hackspace_website_url: str = Field(
+        default="https://leighhack.org", description="URL to the hackspace's website"
+    )
+
+    hackspace_address: str = Field(
+        default="Leigh Hackspace, Unit 3.14, 3rd Floor, Leigh Spinners Mill, Park Lane, Leigh, WN7 2LB, United Kingdom",
+        description="Full address to the hackspace",
+    )
+
+    hackspace_address_lat: float = Field(
+        default=53.493012, description="Latitude of the hackspace"
+    )
+
+    hackspace_address_lon: float = Field(
+        default=-2.493010, description="Longitude of the hackspace"
+    )
+
+    hackspace_timezone: str = Field(
+        default="Europe/London", description="Timezone the hackspace is located in"
+    )
+
+    hackspace_open_entity: str = Field(
+        default="binary_sensor.hackspace_open_multi",
+        description="Entity ID of the Home Assistant device to indicate open status",
+    )
+
+    hackspace_public_calendar: str = Field(
+        default="calendar.public_events",
+        description="The entity ID of the Home Assistant public calendar",
+    )
+
+    hackspace_member_calendar: str = Field(
+        default="calendar.member_events",
+        description="The entity ID of the Home Assistant member calendar",
+    )
+
+    sensors_pressure_enabled: bool = Field(
+        default=False, description="Enable pressure sensors"
+    )
 
 
 settings = Settings()
