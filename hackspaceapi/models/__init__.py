89f2f91972363505a03f3b440d5cf8e33a518347
Andrew Williams <andy@tensixtyone.com>
2023-12-29 14:43:33 +0000

    Fix linting errors

--- hackspaceapi/models/__init__.py
+++ hackspaceapi/models/__init__.py
@@ -1,3 +1,4 @@
-from .config import SettingsModel, SensorSettingsModel
+# ruff: noqa: F401
+from .config import SettingsModel
 from .health import HealthResponseModel
 from .sensors import SensorSettingsModel
